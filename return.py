6e506c9f8cfa3262fe2774d96fc0796822ff964d
yellowpencil30 <1130lwh@gmail.com>
2025-01-06 22:34:59 +0900

    Add files via upload

---
+++ return.py
@@ -0,0 +1,11 @@
+a = '11011'
+b = '00000'
+
+def func(p):
+    print(p)
+    if p == 0:
+        func(p) = '1'
+    else:
+        func(p) = func(p-1) + func(p-1) + '00000' + func(p-1) + func(p-1)
+
+print(func(0))
