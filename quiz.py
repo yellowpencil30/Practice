38a200d65adaeb8dedfb132631fcb84c93b18564
yellowpencil30 <1130lwh@gmail.com>
2025-09-07 17:21:48 +0900

    new quiz

--- quiz.py
+++ quiz.py
@@ -1,76 +1,20 @@
-# 아래와 같은 20×20 격자가 있습니다.
+# 1부터 n까지의 자연수를 차례로 더하여 구해진 값을 삼각수라고 합니다.
+# 예를 들어 7번째 삼각수는 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28이 됩니다.
+# 이런 식으로 삼각수를 구해 나가면 다음과 같습니다.
 
-# 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-# 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-# 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
-# 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
-# 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
-# 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
-# 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
-# 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
-# 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-# 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
-# 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
-# 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
-# 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
-# 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
-# 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
-# 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
-# 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
-# 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
-# 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
-# 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+# 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+# 이 삼각수들의 약수를 구해 봅시다.
 
-# 위에서 대각선 방향으로 연속된 붉은 수 네 개의 곱은 26 × 63 × 78 × 14 = 1788696 입니다.
-# 그러면 수평, 수직, 또는 대각선 방향으로 연속된 수 네 개의 곱 중 최댓값은 얼마입니까?
+#  1: 1
+#  3: 1, 3
+#  6: 1, 2, 3, 6
+# 10: 1, 2, 5, 10
+# 15: 1, 3, 5, 15
+# 21: 1, 3, 7, 21
+# 28: 1, 2, 4, 7, 14, 28
+# 위에서 보듯이, 5개 이상의 약수를 갖는 첫번째 삼각수는 28입니다.
 
-whole_num_list = []
-with open(r'C:\Users\aaa\Desktop\PythonWorkspace\Practice\num.txt', 'r', encoding='utf-8') as f:
-    # 여기가 핵심! 한 줄 씩 리스트로 반환하는 기능이 있음
-    lines = f.readlines()
-    for line in lines:
-        whole_num_list.append((line.strip().split(' ')))
-
-largest = 1
-
-def horizontal_multiplier(list, largest_num):
-    for a in list:
-        for b in range(17):
-            result = 1
-            result *= int(a[b]) * int(a[b+1]) * int(a[b+2]) * int(a[b+3])
-            if result > largest_num:
-                largest_num = result
-    return largest_num
-
-def vertical_multiplier(list, largest_num):
-    for x in range(20):
-        result = 1
-        for y in range(17):
-            result = int(list[y][x]) * int(list[y+1][x]) * int(list[y+2][x]) * int(list[y+3][x])
-        if result > largest_num:
-            largest_num = result
-    return largest_num
-
-# 우하향 대각선
-def diagonal_multiplier(list, largest_num):
-    for A in range(17):
-        for a in range(17):
-            result = int(list[A][a]) * int(list[A+1][a+1]) * int(list[A+2][a+2]) * int(list[A+3][a+3])
-            if result > largest_num:    
-                largest_num = result
-    return largest_num
-
-def diagonal_multiplier_2(list, largest_num):
-    for A in range(17):
-        for a in range(19, 2, -1):
-            result = int(list[A][a]) * int(list[A+1][a-1]) * int(list[A+2][a-2]) * int(list[A+3][a-3])
-            if result > largest_num:    
-                largest_num = result
-    return largest_num
+# 그러면 500개 이상의 약수를 갖는 가장 작은 삼각수는 얼마입니까?
 
 
-HLN = horizontal_multiplier(whole_num_list, largest)
-VLN = vertical_multiplier(whole_num_list, HLN)
-DLN = diagonal_multiplier(whole_num_list, VLN)
-DLN_2 = diagonal_multiplier_2(whole_num_list, DLN)
-print(DLN_2)
+# 결국 등차수열 합 구해서 / 인수분해 하라는 거 아님?
