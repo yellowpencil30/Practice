67eedcf5d3517a7c72ecdce7da5700853fe2c59d
yellowpencil30 <1130lwh@gmail.com>
2025-09-25 02:39:23 +0900

    deep learning

--- quiz.py
+++ quiz.py
@@ -1,16 +1,4 @@
-#세 자연수 a, b, c 가 피타고라스 정리 a2 + b2 = c2 를 만족하면 피타고라스 수라고 부릅니다 (여기서 a < b < c ).
-#예를 들면 32 + 42 = 9 + 16 = 25 = 52이므로 3, 4, 5는 피타고라스 수입니다.
-#a + b + c = 1000 인 피타고라스 수 a, b, c는 한 가지 뿐입니다. 이 때, a × b × c 는 얼마입니까?
-
-
-# 3,4,5 // 6, 8, 10
-# a + b > c
-# c > 334
-# 삼각형이랑 연결해볼까? 너무 돌아가는 것 같긴 한데..
-
-def find_pytha(a,b):
-    if 1000*a + 1000*b - a*b = 500000
-        print(f"{a,b}는 맞아요! c는{1000-a-b}이고 답은 {a*b*c}!")
-    else:
-        print("아니에요")
-        return False
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+import matplotlib.pyplot as plt
